19cd53b6ee7dce02aed3f12b6eab21c5bed6014d
marcinm <marcin.majzel@gmail.com>
2024-04-24 21:34:26 +0200

    day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "W";
+var age = 18;
+
+//1
+if (sex == "W" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+//2
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+//3
+else if (sex == "M" && age < 18) 
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+//4 - nic
+
